31922a5cfb10806bd55c6c3c8a5f1d2b2e0a9e7d
Patrick Delifer <pdelifer@gmail.com>
2022-07-19 11:42:51 -0400

    commite

--- index.js
+++ index.js
@@ -4,134 +4,163 @@ const { exec } = require("@actions/exec");
 const semver = require("semver");
 
 async function run() {
-  try {
-  
-    let currentVersionTag = await getCurrentTag();
+    try {
+        let prerelease = getInput("prerelease", { required: false });
 
-    console.log(`Currrent TAG is ${currentVersionTag}`);
-     
+        let currentVersionTag = await getCurrentTag();
 
-    setOutput("version", nextVersion);
-  } catch (error) {
-    setFailed(error.message);
-  }
+        if (currentVersionTag) {
+            console.log(`Already at version ${currentVersionTag}, skipping...`);
+            setOutput("version", currentVersionTag);
+            return;
+        }
+
+        let nextVersion = await getNextVersionTag({ prerelease });
+        console.log(`Next version: ${nextVersion}`);
+
+        await exec(`git tag ${nextVersion}`);
+
+        try {
+            await execGetOutput(`git push origin ${nextVersion}`);
+        } catch (error) {
+            let errorMessage = `${error}`;
+
+            if (
+                !errorMessage.includes("reference already exists") &&
+                !errorMessage.includes(
+                    "Updates were rejected because the tag already exists in the remote."
+                ) &&
+                !errorMessage.includes("shallow update not allowed")
+            ) {
+                throw error;
+            }
+
+            console.log(
+                `It seems the version ${nextVersion} was already created on origin in the meanwhile, skipping...`
+            );
+        }
+
+        setOutput("version", nextVersion);
+    } catch (error) {
+        setFailed(error.message);
+    }
 }
 
 run();
 
 async function getCurrentTag() {
-  await exec("git fetch --tags");
+    await exec("git fetch --tags");
 
-  // First Check if there is already a release tag at the head...
-  let currentTags = await execGetOutput(`git tag --points-at ${context.sha}`);
+    // First Check if there is already a release tag at the head...
+    let currentTags = await execGetOutput(`git tag --points-at ${context.sha}`);
 
-  return currentTags.map(processVersion).filter(Boolean)[0];
+    return currentTags.map(processVersion).filter(Boolean)[0];
 }
 
 async function getNextVersionTag({ prerelease }) {
-  let allTags = await execGetOutput("git tag");
+    let allTags = await execGetOutput("git tag");
 
-  let previousVersionTags = allTags
-    .map(processVersion)
-    .filter(Boolean)
-    .sort((a, b) => a.localeCompare(b));
+    let previousVersionTags = allTags
+        .map(processVersion)
+        .filter(Boolean)
+        .sort((a, b) => a.localeCompare(b));
 
-  return prerelease
-    ? getPrereleaseVersion(previousVersionTags, prerelease)
-    : getNextDateVersion(previousVersionTags);
+    return prerelease
+        ? getPrereleaseVersion(previousVersionTags, prerelease)
+        : getNextDateVersion(previousVersionTags);
 }
 
 function getNextDateVersion(previousVersionTags) {
-  let { year, month } = getDateParts();
-  let newVersionParts = [`${year}`, `${month}`, 0];
+    let { year, month } = getDateParts();
+    let newVersionParts = [`${year}`, `${month}`, 0];
 
-  while (_tagExists(newVersionParts, previousVersionTags)) {
-    newVersionParts[2]++;
-  }
+    while (_tagExists(newVersionParts, previousVersionTags)) {
+        newVersionParts[2]++;
+    }
 
-  return newVersionParts.join(".");
+    return newVersionParts.join(".");
 }
 
 function getPrereleaseVersion(previousVersionTags, prerelease) {
-  let nextVersion = getNextDateVersion(previousVersionTags);
-  let nextVersionParts = nextVersion.split(".");
+    let nextVersion = getNextDateVersion(previousVersionTags);
+    let nextVersionParts = nextVersion.split(".");
 
-  let prereleaseVersion = 0;
-  while (
-    _tagExists(nextVersionParts, previousVersionTags, [
-      prerelease,
-      prereleaseVersion,
-    ])
-  ) {
-    prereleaseVersion++;
-  }
+    let prereleaseVersion = 0;
+    while (
+        _tagExists(nextVersionParts, previousVersionTags, [
+            prerelease,
+            prereleaseVersion,
+        ])
+    ) {
+        prereleaseVersion++;
+    }
 
-  return `${nextVersion}-${prerelease}.${prereleaseVersion}`;
+    return `${nextVersion}-${prerelease}.${prereleaseVersion}`;
 }
 
 function _tagExists(tagParts, previousVersionTags, prereleaseParts) {
-  let newTag = tagParts.join(".");
+    let newTag = tagParts.join(".");
 
-  if (prereleaseParts) {
-    let [prerelease, prereleaseVersion] = prereleaseParts;
-    newTag = `${newTag}-${prerelease}.${prereleaseVersion}`;
-  }
+    if (prereleaseParts) {
+        let [prerelease, prereleaseVersion] = prereleaseParts;
+        newTag = `${newTag}-${prerelease}.${prereleaseVersion}`;
+    }
 
-  return previousVersionTags.find((tag) => tag === newTag);
+    return previousVersionTags.find((tag) => tag === newTag);
 }
 
 function processVersion(version) {
-  if (!semver.valid(version)) {
-    return false;
-  }
+    if (!semver.valid(version)) {
+        return false;
+    }
 
-  let {
-    major,
-    minor,
-    patch,
-    prerelease,
-    version: parsedVersion,
-  } = semver.parse(version);
+    let {
+        major,
+        minor,
+        patch,
+        prerelease,
+        version: parsedVersion,
+    } = semver.parse(version);
 
-  let { year: currentYear, month: currentMonth } = getDateParts();
+    let { year: currentYear, month: currentMonth } = getDateParts();
 
-  if (major !== currentYear || minor !== currentMonth) {
-    return false;
-  }
+    if (major !== currentYear || minor !== currentMonth) {
+        return false;
+    }
 
-  return parsedVersion;
+    return parsedVersion;
 }
 
 function getDateParts() {
-  let date = new Date();
-  let year = date.getUTCFullYear().toString().substr(-2) * 1;
-  let month = date.getUTCMonth() + 1;
+    let date = new Date();
+    let year = date.getUTCFullYear().toString().substr(-2) * 1;
+    let month = date.getUTCMonth() + 1;
 
-  return { year, month };
+    return { year, month };
 }
 
 async function execGetOutput(command) {
-  let collectedOutput = [];
-  let collectedErrorOutput = [];
+    let collectedOutput = [];
+    let collectedErrorOutput = [];
 
-  let options = {
-    listeners: {
-      stdout: (data) => {
-        let output = data.toString().split("\n");
-        collectedOutput = collectedOutput.concat(output);
-      },
-      stderr: (data) => {
-        let output = data.toString().split("\n");
-        collectedErrorOutput = collectedErrorOutput.concat(output);
-      },
-    },
-  };
+    let options = {
+        listeners: {
+            stdout: (data) => {
+                let output = data.toString().split("\n");
+                collectedOutput = collectedOutput.concat(output);
+            },
+            stderr: (data) => {
+                let output = data.toString().split("\n");
+                collectedErrorOutput = collectedErrorOutput.concat(output);
+            },
+        },
+    };
 
-  try {
-    await exec(command, [], options);
-  } catch (error) {
-    throw new Error(collectedErrorOutput);
-  }
+    try {
+        await exec(command, [], options);
+    } catch (error) {
+        throw new Error(collectedErrorOutput);
+    }
 
-  return collectedOutput;
+    return collectedOutput;
 }
